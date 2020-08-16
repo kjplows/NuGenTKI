ea2ebf24d707eb1f35843aa5b8f21706ed8a23b5
Xianguo Lu <lu.xianguo@gmail.com>
2020-08-16 16:02:31 -0500

    added anaGenerator/GiBUU

---
+++ anaGenerator/GiBUU/doallAna.sh
@@ -0,0 +1,186 @@
+export         GFS0PIa9nuH=
+#"list/GiBUUMINERvA_LE_Neutrino_more_ensembles_H_Hydrogen_1000files.list"
+export         GFS0PIa9nuC="list/GiBUUMINERvA_LE_Neutrino_more_ensembles_Carbon_250files.list"
+export         GFS0PIa9nuNOFSIC=
+#"list/GiBUUMINERvA_LE_Neutrino_NoFSI_Carbon_250files.list"
+
+export GFSPIZEROa7nuH=${GFS0PIa9nuH}
+export GFSPIZEROa7nuC=${GFS0PIa9nuC}
+export GFSPIZEROa7nuNOFSIC=${GFS0PIa9nuNOFSIC}
+
+export EXCL3a10nuH=
+#"list/Filelist_DUNE_nu_Hydrogen_800.list"
+export EXCL3a10nubarH=
+#"list/Filelist_DUNE_nubar_Hydrogen_800.list"
+export EXCL3a10nuC=
+#"list/Filelist_DUNE_nu_T0_Carbon_800.list"
+export EXCL3a10nubarC=
+#"list/Filelist_DUNE_nubar_T0_Carbon_800.list"
+
+export       LOWRECOILa1nuC=""
+export LOWRECOILa1nubarC=""
+export       LOWRECOILa1nuH=""
+export LOWRECOILa1nubarH=""
+
+export     NUBAR1PIa2nubarC=""
+export     NUBAR1PIa2nubarH=""
+
+export           MMECCQEa4nuC=""
+#"list/GiBUUMINERvA_ME_Carbon.list"
+
+export GFSa3nuC=""
+#"list/noEuCut/FinalEventsList_005_T2K_nu_freeDelta_T0_FSI_Flux9_noEuCut_1kruns_1000jobs.log"
+
+export NUGASa8P50=
+#"list/EuCut2to6/eventlist_005_MINERvA_nubar_freeDelta_T0_noFSI_Flux25_EuCut2to6_hydrogen_1000runs_id1007_1000jobs.log"
+
+nfile=
+
+tag=MINERvAGiBUU_LE_
+#MINERvAGiBUU_PIZEROrep_${nfile}_
+#DUNEGiBUU_${nfile}_
+#MINERvAGiBUU_protectedBkg_${nfile}_
+#MINERvAGiBUU_PiCut2Fixed_${nfile}_
+
+####################################
+
+mkexe.sh anaGenerator
+
+#GFS 7
+#opt=${tag}GFSPIZEROa7nuH;        nohup ./doAna.sh $GFSPIZEROa7nuH        ${opt} 7 ${nfile} > see${opt}.log &
+ opt=${tag}GFSPIZEROa7nuC;        nohup ./doAna.sh $GFSPIZEROa7nuC        ${opt} 7 ${nfile} > see${opt}.log &
+#opt=${tag}GFSPIZEROa7nuNOFSIC;   nohup ./doAna.sh $GFSPIZEROa7nuNOFSIC   ${opt} 7 ${nfile} > see${opt}.log &
+
+#exit
+
+#GFS0PI 9
+#opt=${tag}GFS0PIa9nuH;              nohup ./doAna.sh $GFS0PIa9nuH               ${opt} 9 ${nfile} > see${opt}.log & 
+ opt=${tag}GFS0PIa9nuC;              nohup ./doAna.sh $GFS0PIa9nuC               ${opt} 9 ${nfile} > see${opt}.log & 
+#opt=${tag}GFS0PIa9nuNOFSIC;         nohup ./doAna.sh $GFS0PIa9nuNOFSIC          ${opt} 9 ${nfile} > see${opt}.log & 
+
+exit
+
+#EXCL3 10
+
+opt=${tag}EXCL3a10nuH;           nohup ./doAna.sh $EXCL3a10nuH           ${opt} 10 ${nfile} > see${opt}.log &
+opt=${tag}EXCL3a10nubarH;        nohup ./doAna.sh $EXCL3a10nubarH        ${opt} 10 ${nfile} > see${opt}.log &
+opt=${tag}EXCL3a10nuC;           nohup ./doAna.sh $EXCL3a10nuC           ${opt} 10 ${nfile} > see${opt}.log &
+opt=${tag}EXCL3a10nubarC;        nohup ./doAna.sh $EXCL3a10nubarC        ${opt} 10 ${nfile} > see${opt}.log &
+
+exit
+
+
+#NUGAS 8
+opt=${tag}NUGASa8P50;         nohup ./doAna.sh $NUGASa8P50         ${opt} 8 ${nfile} > see${opt}.log &
+
+exit
+
+
+#MMECCQE 4 only carbon
+opt=${tag}MMECCQEa4nuC;        nohup ./doAna.sh $MMECCQEa4nuC        ${opt} 4 ${nfile} > see${opt}.log & 
+
+exit
+
+#GFS 3
+opt=${tag}GFSa3nuC;          nohup ./doAna.sh $GFSa3nuC          ${opt} 3 > see${opt}.log &
+
+exit
+
+
+#LOWRECOIL 1
+opt=${tag}LOWRECOILa1nuC;    nohup ./doAna.sh $LOWRECOILa1nuC    ${opt} 1 > see${opt}.log &
+opt=${tag}LOWRECOILa1nubarC; nohup ./doAna.sh $LOWRECOILa1nubarC ${opt} 1 > see${opt}.log &
+opt=${tag}LOWRECOILa1nuH;    nohup ./doAna.sh $LOWRECOILa1nuH    ${opt} 1 > see${opt}.log &
+opt=${tag}LOWRECOILa1nubarH; nohup ./doAna.sh $LOWRECOILa1nubarH ${opt} 1 > see${opt}.log &
+
+#NUBAR1PI 2
+opt=${tag}NUBAR1PIa2nubarC;  nohup ./doAna.sh $NUBAR1PIa2nubarC  ${opt} 2 > see${opt}.log &
+opt=${tag}NUBAR1PIa2nubarH;  nohup ./doAna.sh $NUBAR1PIa2nubarH  ${opt} 2 > see${opt}.log &
+  
+exit
+
+##################################################################################################################################################
+#
+##################################################################################################################################################
+
+#nohup ./doAna.sh list/noEuCut/FinalEventsList_005_T2K_nu_freeDelta_T0_FSI_Flux9_noEuCut_1kruns_1000jobs.log       nuCarbonT2KT0GiBUU2019a0xBj 0 > seenuCarbonT2KT0GiBUU2019a0xBj.log &
+#nohup ./doAna.sh list/noEuCut/FinalEventsList_005_T2K_nu_freeDelta_T0_noFSInoFSInoFSInoFSI_Flux9_noEuCut_1kruns_1000jobs.log nuNOFSINOFSIcarbonT2KT0GFS1piGiBUU2019v1a0 0 > seenuNOFSINOFSIcarbonT2KT0GFS1piGiBUU2019v1a0.log &
+
+exit
+
+#GFS 3 carbon T2K T0
+
+nohup ./doAna.sh list/noEuCut/FinalEventsList_005_T2K_nu_freeDelta_T0_noFSInoFSInoFSInoFSI_Flux9_noEuCut_1kruns_1000jobs.log nuNOFSINOFSIcarbonT2KT0GiBUU2019a3exp3trackphasespaceNoMeson 3 > seenuNOFSINOFSIcarbonT2KT0GiBUU2019a3exp3trackphasespaceNoMeson.LOG &
+#nohup ./doAna.sh list/noEuCut/FinalEventsList_005_T2K_nu_freeDelta_T0_FSI_Flux9_noEuCut_1kruns_1000jobs.log nuCarbonT2KT0GiBUU2019a3exp3trackphasespaceNoMeson                            3 > seenuCarbonT2KT0GiBUU2019a3exp3trackphasespaceNoMeson.log &
+
+exit
+
+
+#NUBAR1PI 2 sigmaEnu
+
+nohup ./doAna.sh list/sigmaEnu/eventlist_005_MINERvA_nubar_freeDelta_T0_Flux25_carbonEuCut_1kruns_77jobs.log                 nubarCarbonNUBAR1PIsigmaEnuv10a2                     2 > seenubarCarbonNUBAR1PIsigmaEnuv10a2.log &
+nohup ./doAna.sh list/sigmaEnu/eventlist_005_MINERvA_nubar_freeDelta_T0_Flux25_hydrogenEuCut_1kruns_77jobs.log               nubarHydrogenNUBAR1PIsigmaEnuv10a2                   2 > seenubarHydrogenNUBAR1PIsigmaEnuv10a2.log &
+nohup ./doAna.sh list/sigmaEnu/eventlist_005_MINERvA_nubar_freeDelta_T0_Flux25_noFSInoFSInoFSI_carbonEuCut_1kruns_77jobs.log nubarCarbonnoFSInoFSInoFSInoFSINUBAR1PIsigmaEnuv10a2 2 > seenubarCarbonnoFSInoFSInoFSInoFSINUBAR1PIsigmaEnuv10a2.log &
+
+exit
+
+#NUBAR1PI 2  
+
+nohup ./doAna.sh list/EuCut1500to10000/005_MINERvA_nubar_freeDelta_T0_noFSInoFSInoFSInoFSI_Flux25_EuCut1500to10000_1kruns_999jobs.log nubarCarbonnoFSInoFSInoFSInoFSINUBAR1PIv9a2 2 > seenubarCarbonnoFSInoFSInoFSInoFSINUBAR1PIv9a2.log & 
+nohup ./doAna.sh $NUBAR1PIa2nubarC                  nubarCarbonNUBAR1PIv9a2 2                     > seenubarCarbonNUBAR1PIv9a2.log &
+nohup ./doAna.sh $NUBAR1PIa2nubarH      nubarHydrogenNUBAR1PIv9a2 2                   > seenubarHydrogenNUBAR1PIv9a2.log &
+
+exit
+
+
+#LOWRECOIL 1 2019-06-13 GiBUU2019
+
+nohup ./doAna.sh $LOWRECOILa1nuC                        nuCarbonLOWRECOILv8a1GiBUU2019 1 >      seenuCarbonLOWRECOILv8a1GiBUU2019.log &
+nohup ./doAna.sh $LOWRECOILa1nuH             nuHydrogenLOWRECOILv8a1GiBUU2019 1 >    seenuHydrogenLOWRECOILv8a1GiBUU2019.log &
+nohup ./doAna.sh $LOWRECOILa1nubarC                   nubarCarbonLOWRECOILv8a1GiBUU2019 1 >   seenubarCarbonLOWRECOILv8a1GiBUU2019.log &
+nohup ./doAna.sh $LOWRECOILa1nubarH  nubarHydrogenLOWRECOILv8a1GiBUU2019 1 > seenubarHydrogenLOWRECOILv8a1GiBUU2019.log &
+
+exit
+
+#LOWRECOIL 1 v14 flux cut fixed, 2019-01-13 with lineR cut for protons and all pions
+
+nohup ./doAna.sh list/EuCut2to6/FinalEventList_n_MINERvA_nubar_freeDelta_T0_noFSI_Flux14_EuCut2to6_hydrogen_1000runs_1kjobs_15.log  nubarHydrogenLOWRECOILv14a1cutR 1 > seenubarHydrogenLOWRECOILv14a1cutR.log &
+nohup ./doAna.sh list/EuCut2to6/FinalEventList_n_MINERvA_nubar_freeDelta_T0_FSI_Flux14_EuCut2to6_1000runs_992jobs_14.log            nubarCarbonLOWRECOILv14a1cutR 1 > seenubarCarbonLOWRECOILv14a1cutR.log &
+nohup ./doAna.sh list/EuCut2to6/FinalEventList_n_MINERvA_nu_freeDelta_T0_FSI_Flux25_EuCut2to6_1kruns_935jobs_14.log             nuCarbonLOWRECOILv14a1cutR 1 > seenuCarbonLOWRECOILv14a1cutR.log &
+nohup ./doAna.sh list/EuCut2to6/FinalEventList_n_MINERvA_nu_freeDelta_T0_noFSI_Flux25_EuCut2to6_hydrogen_1kruns_1kjobs_14.log   nuHydrogenLOWRECOILv14a1cutR 1 > seenuHydrogenLOWRECOILv14a1cutR.log &
+
+exit
+
+#LOWRECOIL v21 CugnonFSI
+
+nohup ./doAna.sh   list/EuCut2to6/CugnonFSI/FinalEventList_n_MINERvA_nubar_freeDelta_T0_FSI_Flux14_EuCut2to6_1000runs_CugnonFSI_930jobs.log   nubarCarbonLOWRECOILCugnonFSIv21a1 1 > seenubarCarbonLOWRECOILCugnonFSIv21a1.log &
+nohup ./doAna.sh   list/EuCut2to6/CugnonFSI/FinalEventList_n_MINERvA_nu_freeDelta_T0_FSI_Flux25_EuCut2to6_1kruns_CugnonFSI_892jobs.log        nuCarbonLOWRECOILCugnonFSIv21a1    1 > seenuCarbonLOWRECOILCugnonFSIv21a1.log &
+
+exit
+
+#GFS 0 nubar
+
+nohup ./doAna.sh list/FinalEventList_n_MINERvA_nubar_freeDelta_T0_noFSI_Flux14_noEuCut_hydrogen_1000runs_1kjobs_17.log nubarHydrogenGFSv16a0 0 > seenubarHydrogenGFSv16a0.log &
+nohup ./doAna.sh list/FinalEventList_n_MINERvA_nubar_freeDelta_T0_FSI_Flux14_noEuCut_1000runs_999jobs_17.log           nubarCarbonGFSv16a0   0 > seenubarCarbonGFSv16a0.log &
+
+exit
+
+#LOWRECOIL 1 v16 noFSI
+
+nohup ./doAna.sh list/FinalEventList_n_MINERvA_nubar_freeDelta_T0_noFSInoFSInoFSInoFSI_Flux14_EuCut2to6_1000runs_1kjobs_16.log nubarCarbonLOWRECOILnoFSInoFSInoFSInoFSIv15a1 1 > seenubarCarbonLOWRECOILnoFSInoFSInoFSInoFSIv15a1.log &
+nohup ./doAna.sh list/FinalEventList_n_MINERvA_nu_freeDelta_T0_noFSInoFSInoFSI_Flux25_EuCut2to6_1kruns_1kjobs_16.log           nuCarbonLOWRECOILnoFSInoFSInoFSInoFSIv15a1 1 > seenuCarbonLOWRECOILnoFSInoFSInoFSInoFSIv15a1.log &
+
+exit
+
+#LOWRECOIL 1 v14 flux cut fixed                                                                                                                                                                                                    
+
+nohup ./doAna.sh list/FinalEventList_n_MINERvA_nubar_freeDelta_T0_noFSI_Flux14_EuCut2to6_hydrogen_1000runs_1kjobs_15.log  nubarHydrogenLOWRECOILv14a1 1 > seenubarHydrogenLOWRECOILv14a1.log &
+nohup ./doAna.sh list/FinalEventList_n_MINERvA_nubar_freeDelta_T0_FSI_Flux14_EuCut2to6_1000runs_992jobs_14.log            nubarCarbonLOWRECOILv14a1 1 > seenubarCarbonLOWRECOILv14a1.log &
+nohup ./doAna.sh list/FinalEventList_n_MINERvA_nu_freeDelta_T0_FSI_Flux25_EuCut2to6_1kruns_935jobs_14.log             nuCarbonLOWRECOILv14a1 1 > seenuCarbonLOWRECOILv14a1.log &
+nohup ./doAna.sh list/FinalEventList_n_MINERvA_nu_freeDelta_T0_noFSI_Flux25_EuCut2to6_hydrogen_1kruns_1kjobs_14.log   nuHydrogenLOWRECOILv14a1 1 > seenuHydrogenLOWRECOILv14a1.log &
+
+exit
+
+
+
+
