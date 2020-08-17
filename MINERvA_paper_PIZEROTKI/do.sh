0283bfa0540af543a2837e43a96adc8d57a69b1e
Xianguo Lu <lu.xianguo@gmail.com>
2020-08-17 13:42:24 +0100

    updated PI0TKIpaper do.sh

--- MINERvA_paper_PIZEROTKI/do.sh
+++ MINERvA_paper_PIZEROTKI/do.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-echo README
-echo run the following first
-echo go to style and do.sh
-echo source setup.sh first
-echo
-echo
-
-which compile_exe.sh
-
 mkdir -p outplot/final
 
 for aa in  mcchi2
            #draw 
 do
 
-    compile_exe.sh ${aa} -lstyle -Istyle -Lstyle
+    mkexe.sh ${aa} -lstyle -I${NUGENTKI}/style -L${NUGENTKI}/style
     
     ./${aa} > see${aa}.log
 
